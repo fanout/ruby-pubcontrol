ad6bfdfea55a511d73274c2477d70065f1990d71
Kon <bokarius@comcast.net>
2015-03-12 13:41:44 -0700

    added remaining tests and documentation

--- lib/pubcontrol.rb
+++ lib/pubcontrol.rb
@@ -10,7 +10,15 @@ require_relative 'item.rb'
 require_relative 'pubcontrolclient.rb'
 require_relative 'pcccbhandler.rb'
 
+# The PubControl class allows a consumer to manage a set of publishing
+# endpoints and to publish to all of those endpoints via a single publish
+# or publish_async method call. A PubControl instance can be configured
+# either using a hash or array of hashes containing configuration information
+# or by manually adding PubControlClient instances.
 class PubControl
+
+  # Initialize with or without a configuration. A configuration can be applied
+  # after initialization via the apply_config method.
   def initialize(config=nil)
     @clients = Array.new
     if !config.nil?
@@ -18,14 +26,21 @@ class PubControl
     end
   end
 
+  # Remove all of the configured PubControlClient instances.
   def remove_all_clients
     @clients = Array.new
   end
 
+  # Add the specified PubControlClient instance.
   def add_client(client)
     @clients.push(client)
   end
 
+  # Apply the specified configuration to this PubControl instance. The
+  # configuration object can either be a hash or an array of hashes where
+  # each hash corresponds to a single PubControlClient instance. Each hash
+  # will be parsed and a PubControlClient will be created either using just
+  # a URI or a URI and JWT authentication information.
   def apply_config(config)
     if !config.is_a?(Array)
       config = [config]
@@ -39,18 +54,29 @@ class PubControl
     end
   end
 
+  # The finish method is a blocking method that ensures that all asynchronous
+  # publishing is complete for all of the configured PubControlClient
+  # instances prior to returning and allowing the consumer to proceed.
   def finish
     @clients.each do |pub|
       pub.finish
     end
   end
 
+  # The synchronous publish method for publishing the specified item to the
+  # specified channel for all of the configured PubControlClient instances.
   def publish(channel, item)
     @clients.each do |pub|
       pub.publish(channel, item)
     end
   end
 
+  # The asynchronous publish method for publishing the specified item to the
+  # specified channel on the configured endpoint. The callback method is
+  # optional and will be passed the publishing results after publishing is
+  # complete. Note that a failure to publish in any of the configured
+  # PubControlClient instances will result in a failure result being passed
+  # to the callback method along with the first encountered error message.
   def publish_async(channel, item, callback=nil)
     cb = nil
     if !callback.nil?
---
+++ tests/pubcontrol_tests.rb
@@ -0,0 +1,158 @@
+require 'format'
+require 'item'
+require 'pubcontrol'
+require 'minitest/autorun'
+
+class PubControlClientTestClass
+  attr_accessor :was_finish_called
+  attr_accessor :publish_channel
+  attr_accessor :publish_item
+  attr_accessor :publish_callback
+
+  def initialize
+    @was_finish_called = false
+    @publish_channel = nil
+    @publish_item = nil
+    @publish_callback = nil
+  end
+
+  def finish
+    @was_finish_called = true
+  end
+
+  def publish(channel, item)
+    @publish_channel = channel
+    @publish_item = item
+  end
+
+  def publish_async(channel, item, callback=nil)
+    @publish_channel = channel
+    @publish_item = item
+    @publish_callback = callback
+  end
+end
+
+class TestPubControl < Minitest::Test
+  def test_initialize
+    pc = PubControl.new
+    assert_equal(pc.instance_variable_get(:@clients).length, 0)
+    config = {'uri' => 'uri', 'iss' => 'iss', 'key' => 'key'}
+    pc = PubControl.new(config)
+    assert_equal(pc.instance_variable_get(:@clients).length, 1)
+    config = [{'uri' => 'uri', 'iss' => 'iss', 'key' => 'key'},
+        {'uri' => 'uri', 'iss' => 'iss', 'key' => 'key'}]
+    pc = PubControl.new(config)
+    assert_equal(pc.instance_variable_get(:@clients).length, 2)
+  end
+
+  def test_remove_all_clients
+    pc = PubControl.new
+    pc.instance_variable_get(:@clients).push("client")
+    pc.remove_all_clients
+    assert_equal(pc.instance_variable_get(:@clients).length, 0)
+  end
+
+  def test_add_client
+    pc = PubControl.new
+    pc.add_client("client")
+    assert_equal(pc.instance_variable_get(:@clients)[0], "client")
+  end
+
+  def test_apply_config
+    pc = PubControl.new
+    config = [{'uri' => 'uri'},
+        {'uri' => 'uri1', 'iss' => 'iss1', 'key' => 'key1'},
+        {'uri' => 'uri2', 'iss' => 'iss2', 'key' => 'key2'}]
+    pc.apply_config(config)
+    assert_equal(pc.instance_variable_get(
+        :@clients)[0].instance_variable_get(:@uri), "uri")
+    assert_equal(pc.instance_variable_get(
+        :@clients)[0].instance_variable_get(:@auth_jwt_claim), nil)
+    assert_equal(pc.instance_variable_get(
+        :@clients)[0].instance_variable_get(:@auth_jwt_key), nil)
+    assert_equal(pc.instance_variable_get(
+        :@clients)[1].instance_variable_get(:@uri), "uri1")
+    assert_equal(pc.instance_variable_get(
+        :@clients)[1].instance_variable_get(:@auth_jwt_claim),
+        {'iss' => 'iss1'})
+    assert_equal(pc.instance_variable_get(
+        :@clients)[1].instance_variable_get(:@auth_jwt_key), "key1")
+    assert_equal(pc.instance_variable_get(
+        :@clients)[2].instance_variable_get(:@uri), "uri2")
+    assert_equal(pc.instance_variable_get(
+        :@clients)[2].instance_variable_get(:@auth_jwt_claim),
+        {'iss' => 'iss2'})
+    assert_equal(pc.instance_variable_get(
+        :@clients)[2].instance_variable_get(:@auth_jwt_key), "key2")
+  end
+
+  def test_finish
+    pc = PubControl.new
+    pccs = []
+    (0..3).each do |n|
+      pcc = PubControlClientTestClass.new
+      pccs.push(pcc)
+      pc.add_client(pcc)
+    end
+    pc.finish
+    (0..3).each do |n|
+      assert(pccs[n].was_finish_called)
+    end
+  end
+
+  def test_publish
+    pc = PubControl.new
+    pccs = []
+    (0..3).each do |n|
+      pcc = PubControlClientTestClass.new
+      pccs.push(pcc)
+      pc.add_client(pcc)
+    end
+    pc.publish("channel", "item")
+    (0..3).each do |n|
+      assert_equal(pccs[n].publish_channel, "channel")
+      assert_equal(pccs[n].publish_item, "item")
+    end
+  end
+
+  def test_publish_async_without_callback
+    pc = PubControl.new
+    pccs = []
+    (0..3).each do |n|
+      pcc = PubControlClientTestClass.new
+      pccs.push(pcc)
+      pc.add_client(pcc)
+    end
+    pc.publish_async("channel", "item")
+    (0..3).each do |n|
+      assert_equal(pccs[n].publish_channel, "channel")
+      assert_equal(pccs[n].publish_item, "item")
+      assert_equal(pccs[n].publish_callback, nil)
+    end
+  end
+
+  def callback_for_testing(result, error)
+    assert_equal(@has_callback_been_called, false)
+    assert_equal(result, false)
+    assert_equal(error, 'error')
+    @has_callback_been_called = true
+  end
+
+  def test_publish_async_with_callback
+    @has_callback_been_called = false
+    pc = PubControl.new
+    pccs = []
+    (0..3).each do |n|
+      pcc = PubControlClientTestClass.new
+      pccs.push(pcc)
+      pc.add_client(pcc)
+    end
+    pc.publish_async("channel", "item", method(:callback_for_testing))
+    (0..3).each do |n|
+      assert_equal(pccs[n].publish_channel, "channel")
+      assert_equal(pccs[n].publish_item, "item")
+      pccs[n].publish_callback.call(false, 'error')
+    end
+    assert(@has_callback_been_called)
+  end
+end
